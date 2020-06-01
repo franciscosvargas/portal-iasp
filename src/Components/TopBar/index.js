012000a58b316d66ba5d5fffd94fb95c41a7d833
Francisco Vargas <falisson.sv@gmail.com>
2020-06-01 11:54:00 -0300

    Changes

--- src/Components/TopBar/index.js
+++ src/Components/TopBar/index.js
@@ -1,31 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Container, Logo, Right, LinkR, MenuButton, Mobile, LinkMobile } from './styles'
+import { Container, Logo, Right, Link, MenuButton, LinkM, Mobile } from './styles'
 
 import logoIasp from '../../assets/logo.png' 
 import menuIcon from '../../assets/menu.svg'
+import closeIcon from '../../assets/close.svg'
 
 export default function TopBar() {
+	const [mobile, setMobile] = useState(false)
+
 	return (
 		<Container>
-			<Logo src={logoIasp}/>
+			<div style={{display: 'flex', width: '100%'}}>
+				
+				<Logo src={logoIasp}/>
 
-			<Right>
-				<LinkR to="/">INÍCIO</LinkR>
-				<LinkR to="#">QUEM SOMOS</LinkR>
-				<LinkR to="#">CONVÊNIOS</LinkR>
-				<LinkR to="/contato">CONTATO</LinkR>
-			</Right>
+				<Right>
+					<Link href="/">INÍCIO</Link>
+					<Link href="/">QUEM SOMOS</Link>
+					<Link href="/">CONVÊNIOS</Link>
+					<Link href="/">CONTATO</Link>
+
+					<MenuButton onClick={() => setMobile(!mobile)} src={mobile ? closeIcon : menuIcon} />
+				</Right>
+			</div>
+
+			{mobile && (
+				<Mobile>
+						<LinkM href="/">INÍCIO</LinkM>
+						<LinkM href="/">QUEM SOMOS</LinkM>
+						<LinkM href="/">CONVÊNIOS</LinkM>
+						<LinkM href="/">CONTATO</LinkM>
+				</Mobile>
+			)}
 
 
-			<Mobile>
-				<LinkMobile href="/">INÍCIO</LinkMobile>
-				<LinkMobile href="#">QUEM SOMOS</LinkMobile>
-				<LinkMobile href="#">CONVÊNIOS</LinkMobile>
-				<LinkMobile href="/contato">CONTATO</LinkMobile>
-			</Mobile>
-
-			
 		</Container>
 	);
 }
--- src/Components/TopBar/styles.js
+++ src/Components/TopBar/styles.js
@@ -1,27 +1,20 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom'
 
 export const Container = styled.div`
 	width: calc(100% - 100px);
 	height: 80%;
 	padding: 40px 50px 0px 50px;
 	display: flex;
-
+	flex-direction: column;
 	@media (max-width: 845px) {
-		width: 100%;
-		padding: 40px 0px 0 0px;
-		flex-direction: column;
+		width: calc(100% - 40px);
+		padding: 40px 20px 0 15px;
 	}
 `;
 
 export const Logo = styled.img`
 	width: 210px;
 	height: 84px;
-
-	@media (max-width: 845px) {
-		margin-left: auto;
-		margin-right: auto;
-	}
 `
 
 export const Right = styled.div`
@@ -29,56 +22,48 @@ export const Right = styled.div`
 	display: flex;
 	align-items: center;
 	height: 80px;
-
-	@media (max-width: 845px) {
-		display: none;
-	}
-
+`
+export const Mobile = styled.div`
+	margin-left: auto;
+	display: flex;
+	flex-direction: column;
+	text-align: right;
 `
 
-export const LinkR = styled(Link)`
+export const Link = styled.a`
 	color: #fff;
 	font-family: 'Roboto', sans-serif;
 	margin-left: 30px;
 	font-size: 20px;
 	font-weight: 300;
-
 	:hover {
 		font-weight: 400;
 		opacity: 0.8;
 	}
+	@media (max-width: 845px) {
+		display: none;
+	}
+`
+
+export const LinkM = styled.a`
+	color: #fff;
+	font-family: 'Roboto', sans-serif;
+	margin-top: 20px;
+	font-size: 20px;
+	font-weight: 300;
+	
+	:hover {
+		font-weight: 400;
+		opacity: 0.8;
+	}
+	
 `
 
 export const MenuButton = styled.img`
 	width: 34px;
 	height: 34px;
 	display: none;
-
 	@media (max-width: 845px) {
 		display: flex;
-	}
-`
-
-export const Mobile = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-around;
-	padding: 20px;
-`
-
-export const LinkMobile = styled.a`
-	color: #fff;
-	font-family: 'Roboto', sans-serif;
-	font-size: 20px;
-	font-weight: 300;
-	margin-top: 10px;
-
-	:hover {
-		font-weight: 400;
-		opacity: 0.8;
-	}
-
-	@media (min-width: 845px) {
-		display: none;
 	}
 `
