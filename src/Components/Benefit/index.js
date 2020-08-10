7a1b40a298b32c7da14cbc80ac19db0a5dd19ad0
Francisco Vargas <falisson.sv@gmail.com>
2020-08-10 11:44:47 -0300

    Update image url

--- src/Components/Benefit/index.js
+++ src/Components/Benefit/index.js
@@ -5,7 +5,7 @@ import { Container, Logo, Title, Description, Location } from './styles'
 function Benefit({info, ...rest}) {
 	return (
 		<Container to={`/convenios/${info._id}`}>
-			<Logo src={`http://localhost:3001/files/${info.logo}`} />
+			<Logo src={`https://api.portaliasp.org/files/${info.logo}`} />
 			<Title>{info.title}</Title>
 			<Location>{info.city} - {info.state}</Location>
 			<Description>{info.description}</Description>
