20d71817ff5ed87412f8242520d02c0d8aef2774
Francisco Vargas <falisson.sv@gmail.com>
2020-08-10 10:53:35 -0300

    Implements Hash Router

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
   
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
@@ -8,14 +8,14 @@ import Benefits from './Pages/Benefits';
 import Benefit from './Pages/Benefit';
 
 const Routes = () => (
-    <BrowserRouter>
+    <HashRouter>
 		<Switch>
         	<Route path="/" exact component={Home}/>
 					<Route path="/contato" component={Contact}/>
 					<Route path="/convenios" exact component={Benefits}/>
 					<Route path="/convenios/:id" exact component={Benefit}/>
 		</Switch>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 export default Routes
